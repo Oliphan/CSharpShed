6faa3a612f39e689de3cc8c2bdf2e942ad0eecc1
Liam Robinson <liam.robinson.lmr@gmail.com>
2025-07-07 22:19:56 +0100

    Create DisposeHandlerTests

---
+++ Shed.ResourceManagement.Tests/Disposal/DisposeHandlerTests.cs
@@ -0,0 +1,57 @@
+﻿namespace Shed.ResourceManagement.Tests.Disposal
+{
+	using Shed.ResourceManagement.Disposal;
+
+
+	public class DisposeHandlerTests
+	{
+		private Action<DisposeHandler> onDispose;
+
+		private DisposeHandler handler;
+
+
+		[SetUp]
+		public void SetUp()
+		{
+			onDispose = Substitute.For<Action<DisposeHandler>>();
+
+			handler = new DisposeHandler(onDispose);
+		}
+
+
+		[TearDown]
+		public void TearDown()
+		{
+			handler.Dispose();
+		}
+
+
+		[Test]
+		public void DisposeIsNotCalledEarly()
+			=> Assert.DoesNotThrow(() => onDispose.DidNotReceiveWithAnyArgs().Invoke(default!));
+
+		[Test]
+		public void DisposeCorrectlyCallsOnDisposeCallbackOnFirstInvokation()
+		{
+			// Act
+			handler.Dispose();
+
+			// Assert
+			Assert.DoesNotThrow(() => onDispose.Received(1).Invoke(handler));
+		}
+
+		[Test]
+		public void DisposeDoesNotCallOnDisposeCallbackOnSecondInvokation()
+		{
+			// Arrange
+			handler.Dispose();
+			onDispose.ClearReceivedCalls();
+
+			// Act
+			handler.Dispose();
+
+			// Assert
+			Assert.DoesNotThrow(() => onDispose.DidNotReceiveWithAnyArgs().Invoke(default!));
+		}
+	}
+}
