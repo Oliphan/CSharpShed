dc012773dccdfcc33c7d32a453c7e42291494e1c
Liam Robinson <liam.robinson.lmr@gmail.com>
2026-07-01 22:38:15 +0100

    Add Shed.LateInjection.Godot project

---
+++ Shed.LateInjection.Godot/LateInjectorExtensions.cs
@@ -0,0 +1,81 @@
+﻿using Godot;
+using Shed.LateInjection.Abstractions;
+
+namespace Shed.LateInjection.Godot;
+
+public static class ServiceCollectionExtensions
+{
+    public static async Task<List<Node>> InjectWithPostCallbacks(
+        this ILateInjector lateInjector,
+        Node node)
+    {
+        List<Node> injectedNodes = [];
+        await lateInjector.InjectWithPostCallbacks(node, injectedNodes);
+        return injectedNodes;
+    }
+
+    public static async Task InjectWithPostCallbacks(
+        this ILateInjector lateInjector,
+        Node node,
+        List<Node> injectedNodes)
+    {
+        lateInjector.InjectNode(node, injectedNodes);
+
+        foreach (var injected in injectedNodes)
+        {
+            if (injected is IPostInject postInject)
+            {
+                postInject.PostInject();
+            }
+            if (injected is IPostInjectAsync postInjectAsync)
+            {
+                await postInjectAsync.PostInjectAsync();
+            }
+        }
+    }
+
+    public static List<Node> InjectNode(
+        this ILateInjector lateInjector,
+        Node node)
+    {
+        List<Node> injectedNodes = [];
+        lateInjector.InjectNode(node, injectedNodes);
+        return injectedNodes;
+    }
+
+    public static void InjectNode(
+        this ILateInjector lateInjector,
+        Node node,
+        List<Node> injectedNodes)
+    {
+        lateInjector.Inject(node);
+        injectedNodes.Add(node);
+        lateInjector.InjectChildren(node, injectedNodes);
+    }
+
+    public static List<Node> InjectChildren(
+        this ILateInjector lateInjector,
+        Node node)
+    {
+        List<Node> injectedNodes = [];
+        lateInjector.InjectChildren(node, injectedNodes);
+        return injectedNodes;
+    }
+
+    public static void InjectChildren(
+        this ILateInjector lateInjector,
+        Node node,
+        List<Node> injectedNodes)
+    {
+        foreach (var child in node.GetChildren())
+        {
+            lateInjector.Inject(child);
+            injectedNodes.Add(child);
+        }
+
+        foreach (var child in node.GetChildren())
+        {
+            lateInjector.InjectChildren(child, injectedNodes);
+        }
+    }
+}
